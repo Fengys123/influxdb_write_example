5fa476830d583b0c52131015f9c4ea44bc5e29fb
fys <1113014250@qq.com>
2023-06-26 15:53:09 +0800

    fix: ts

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 	"time"
 
 	"github.com/influxdata/influxdb-client-go/v2"
-	client1 "github.com/influxdata/influxdb1-client/v2"
+	clientv1 "github.com/influxdata/influxdb1-client/v2"
 )
 
 func main() {
@@ -17,8 +17,7 @@ func main() {
 
 // https://github.com/influxdata/influxdb1-client
 func influxdb_v1_write() {
-	// https://docs.influxdata.com/influxdb/v1.7/tools/api_client_libraries/
-	c, err := client1.NewHTTPClient(client1.HTTPConfig{
+	c, err := clientv1.NewHTTPClient(clientv1.HTTPConfig{
 		Addr: "http://localhost:4000/v1/influxdb",
 	})
 	if err != nil {
@@ -26,20 +25,18 @@ func influxdb_v1_write() {
 	}
 	defer c.Close()
 
-	// Create a new point batch
-	bp, _ := client1.NewBatchPoints(client1.BatchPointsConfig{
+	bp, _ := clientv1.NewBatchPoints(clientv1.BatchPointsConfig{
 		Database:  "public",
 		Precision: "s",
 	})
 
-	// Create a point and add to batch
 	tags := map[string]string{"cpu": "cpu-total"}
 	fields := map[string]interface{}{
 		"idle":   10.1,
 		"system": 53.3,
 		"user":   46.6,
 	}
-	pt, err := client1.NewPoint("cpu_usage", tags, fields, time.Now())
+	pt, err := clientv1.NewPoint("cpu_usage", tags, fields, time.Now())
 	if err != nil {
 		fmt.Println("Error: ", err.Error())
 	}
@@ -56,7 +53,7 @@ func influxdb_v2_write() {
 
 	writeAPI := client.WriteAPIBlocking("", "public")
 
-	line := fmt.Sprintf("stat,unit=temperature avg=%f,max=%f", 23.5, 45.0)
+	line := fmt.Sprintf("stat,unit=123 avg=%f,max=%f %d", 23.5, 45.0, time.Now().UnixNano())
 
 	err := writeAPI.WriteRecord(context.Background(), line)
 
